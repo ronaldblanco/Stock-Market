18771a5a32a73449ddaa7e2007dce5d5da0f2250
Ronald Blanco <pnald.blanco@gmail.com>
2017-06-16 13:00:27 +0000

    websocket update time fixed

--- app/controllers/webSocketHandler.server.js
+++ app/controllers/webSocketHandler.server.js
@@ -4,6 +4,16 @@
 var config = require('../models/config.js');
 var numClients = 0;
 // Controller agrees to implement the function called "respond"
+
+function sendAdd(io){
+    io.emit('stats', { numClients: numClients, data: config }); 
+    io.emit('broadcast', 'A new Stock was Add by a Client, please update the chart!');
+}
+function sendDel(io){
+    io.emit('stats', { numClients: numClients, data: config }); 
+    io.emit('broadcast', 'A Stock was remove by a Client, please update the chart!');
+}
+
 module.exports.respond = function(endpoint,socket){
     
     //var publicHandler = require(process.cwd() + '/app/controllers/publicHandler.server.js');
@@ -16,12 +26,19 @@ module.exports.respond = function(endpoint,socket){
         console.log('WEBSOCKETSERVER->'+config.data.datasets.length);
         console.log('A client sent us this message:', data.message);
         if(data.message == 'I did add a stock to the chart!'){
+            //setTimeout(sendAdd(io), 11);
             io.emit('stats', { numClients: numClients, data: config }); 
             io.emit('broadcast', 'A new Stock was Add by a Client, please update the chart!');
         } 
         else if(data.message == 'I did remove a stock from the chart!'){
+            //setTimeout(sendDel(io), 11);
             io.emit('stats', { numClients: numClients, data: config }); 
             io.emit('broadcast', 'A Stock was remove by a Client, please update the chart!');
+        }
+        else if(data.message == 'Send me again please!'){
+            
+            //io.emit('stats', { numClients: numClients, data: config }); 
+            
         }
     });
 	//console.log('socked.io->New Client Connected');
